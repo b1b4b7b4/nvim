7ffcf40ad05e4c35949fbeddc16a0fbe79613a8f
just-noname-bing <danikfedorov41@gmail.com>
2025-04-14 17:42:20 +0300

    guh

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,16 @@
-require('telescope').setup{
- defaults = {
-    file_ignore_patterns = {"node_modules"},
-  }
+require('telescope').setup {
+    defaults = {
+        file_ignore_patterns = { "node_modules" },
+    }
 }
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pw', function()
+    local word = vim.fn.expand("<cword>")
+    builtin.grep_string({ search = word })
+end)
+-- vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
