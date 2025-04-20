e29252616c4542c0afa6a5c4ea2942fec4a2b4d7
just-noname-bing <danikfedorov41@gmail.com>
2025-04-20 08:57:06 +0300

    upd: yes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,7 +10,12 @@ vim.keymap.set('n', '<leader>pw', function()
     local word = vim.fn.expand("<cword>")
     builtin.grep_string({ search = word })
 end)
--- vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+
+-- <C-q> quicklist
+
+-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Telescope live grep' })
+
+-- vim.keymap.set('n', '<leader>ps', function()
+--     builtin.grep_string({ search = vim.fn.input("Grep > ") });
+-- end)
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -29,9 +29,9 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
