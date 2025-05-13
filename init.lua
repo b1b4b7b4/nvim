6a0d1e568a85a4e0f0ca173b36908779692e2f88
just-noname-bing <danikfedorov41@gmail.com>
2025-05-13 10:56:52 +0300

    feat: reb

--- init.lua
+++ init.lua
@@ -782,23 +782,7 @@ require('lazy').setup({
           vim.keymap.set('n', '<leader>p', function()
             vim.cmd.Git 'push'
           end, opts)
-
-          -- rebase always
-          vim.keymap.set('n', '<leader>P', function()
-            -- Prompt the user for a branch name
-            vim.ui.input({ prompt = 'Enter new branch name: ' }, function(branch_name)
-              if branch_name and branch_name ~= '' then
-                -- Run Git commands to create and push the branch
-                vim.cmd('Git checkout -b ' .. branch_name) -- Create new branch
-                vim.cmd('Git push -u origin ' .. branch_name) -- Push branch to origin and set upstream
-              else
-                print 'Branch name cannot be empty!'
-              end
-            end)
-          end, opts)
-
-          -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-          -- needed if i did not set the branch up correctly
+          vim.keymap.set('n', '<leader>P', ':Git pull --rebase', opts)
           vim.keymap.set('n', '<leader>t', ':Git push -u origin ', opts)
         end,
       })
