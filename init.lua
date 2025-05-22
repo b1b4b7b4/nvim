39cb997c491ed2dfed16b99fda9678a50a2eb452
just-noname-bing <danikfedorov41@gmail.com>
2025-05-22 17:28:01 +0300

    save

--- init.lua
+++ init.lua
@@ -212,10 +212,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      -- vim.keymap.set('n', '<leader><leader>', vim.lsp.buf.code_action, { desc = 'Code action' })bu
+      vim.keymap.set('n', '<leader><leader>', vim.lsp.buf.code_action, { desc = 'Code action' })
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader><leader>', function()
+      vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
